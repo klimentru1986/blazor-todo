de025b76f0f15a692be39063108dcbfaa8b117bc
klimentru1986 <kliment1986@inbox.ru>
2019-07-22 07:36:22 +0300

    Form validation

--- ToDo.Models/ToDo/ToDoModel.cs
+++ ToDo.Models/ToDo/ToDoModel.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDo.Models
 {
     public class ToDoModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
         public bool Completed { get; set; }
